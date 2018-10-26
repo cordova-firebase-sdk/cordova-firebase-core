a2faa522b090cdd29fe600447fcead081278b352
wf9a5m75 <wf9a5m75@gmail.com>
2018-10-25 17:46:43 -0700

    Creating test code

--- src/ts/__test__/BaseClass.spec.ts
+++ src/ts/__test__/BaseClass.spec.ts
@@ -1,18 +1,18 @@
-import BaseClass = require('../BaseClass');
+import BaseClass = require("../BaseClass");
 
-describe('basic', () => {
+describe("BaseClass test", () => {
 
-  let instanceA: BaseClass = new BaseClass();
-  let instanceB: BaseClass = new BaseClass();
+  const instanceA: BaseClass = new BaseClass();
+  const instanceB: BaseClass = new BaseClass();
 
-  it('"instanceA.hello" should be "world"', () => {
-    instanceA._set('hello', 'world');
-    expect(instanceA._get('hello')).toEqual('world');
+  it("'instanceA.hello' should be 'world'", () => {
+    instanceA._set("hello", "world");
+    expect(instanceA._get("hello")).toEqual("world");
   });
 
-  it('"instanceB.anotherHello" should be the same as "instanceA.hello"', () => {
-    instanceA._bindTo('hello', instanceB, 'anotherHello');
-    expect(instanceB._get('anotherHello')).toEqual('world');
+  it("'instanceB.anotherHello' should be the same as 'instanceA.hello'", () => {
+    instanceA._bindTo("hello", instanceB, "anotherHello");
+    expect(instanceB._get("anotherHello")).toEqual("world");
   });
 
 });
