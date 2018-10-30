ec28b985f8714055b272e4dd5a8d5f68e5fdf3a8
wf9a5m75 <wf9a5m75@gmail.com>
2018-10-30 12:42:35 -0700

    Reimplement common module in TypeScript

---
+++ src/ts/common.ts
@@ -0,0 +1,75 @@
+/**
+ * Returns true if given `packageName` is available.
+ *
+ * ```ts
+ * isInitialized("firebase.app"); // true if window.firebase.app is available
+ * ```
+ * @param packageName - name space
+ * @returns true if available.
+ */
+export const isInitialized = (packageName: string): boolean => {
+
+  let parent: any = window;
+  const steps: string[] = packageName.split(/\\./);
+  const results: string[] = steps.filter((step: string): boolean => {
+    if (step in parent) {
+      parent = parent[step];
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return results.length === steps.length;
+};
+
+export interface IloadJsPromiseOptions {
+
+  /**
+   * expect JS package name after JS initized.
+   */
+  package: string;
+
+  /**
+   * JS url
+   */
+  url: string;
+}
+
+/**
+ * Load JS file, and wait it is initialized.
+ * @param options - options
+ * @returns Promise<void>
+ */
+export const loadJsPromise = (options: IloadJsPromiseOptions): Promise<void> => {
+  return new Promise<void>((resolve, reject) => {
+    if (isInitialized(options.package)) {
+      resolve();
+    } else {
+      const scriptTag: HTMLScriptElement = document.createElement("src") as HTMLScriptElement;
+      scriptTag.src = options.url;
+      scriptTag.onerror = reject;
+      scriptTag.onload = () => {
+        let timeout: number = 20;
+        const timer = setInterval(() => {
+          timeout--;
+          if (isInitialized(options.package)) {
+            clearInterval(timer);
+            resolve();
+          }
+          if (timeout === 0) {
+            reject(new Error("[Timeout] JS does not initialized in 2 seconds."));
+          }
+        }, 10);
+      };
+      document.body.appendChild(scriptTag);
+    }
+  });
+};
+
+/**
+ * @hidden
+ */
+export const nextTick = (fn) => {
+  Promise.resolve().then(fn);
+};
--- www/common.js
+++ www/common.js
@@ -1,46 +1,62 @@
-
-
-var isInitialized = function(packageName) {
-  var parent = window;
-  var steps = packageName.split(/\./);
-  var results = steps.filter(function(step) {
-    if (step in parent) {
-      parent = parent[step];
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  return results.length === steps.length;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Returns true if given `packageName` is available.
+ *
+ * ```ts
+ * isInitialized("firebase.app"); // true if window.firebase.app is available
+ * ```
+ * @param packageName - name space
+ * @returns true if available.
+ */
+exports.isInitialized = (packageName) => {
+    let parent = window;
+    const steps = packageName.split(/\\./);
+    const results = steps.filter((step) => {
+        if (step in parent) {
+            parent = parent[step];
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    return results.length === steps.length;
 };
-
-var loadJsPromise = function(options) {
-  return new Promise(function(resolve, reject) {
-
-    if (isInitialized(options.package)) {
-      resolve();
-    } else {
-      var scriptTag = document.createElement('script');
-      scriptTag.src = options.url;
-      scriptTag.onload = function() {
-        var timer = setInterval(function() {
-          if (isInitialized(options.package)) {
-            clearInterval(timer);
+/**
+ * Load JS file, and wait it is initialized.
+ * @param options - options
+ * @returns Promise<void>
+ */
+exports.loadJsPromise = (options) => {
+    return new Promise((resolve, reject) => {
+        if (exports.isInitialized(options.package)) {
             resolve();
-          }
-        }, 10);
-      };
-      scriptTag.onerror = reject;
-      document.body.appendChild(scriptTag);
-    }
-  });
+        }
+        else {
+            const scriptTag = document.createElement("src");
+            scriptTag.src = options.url;
+            scriptTag.onerror = reject;
+            scriptTag.onload = () => {
+                let timeout = 20;
+                const timer = setInterval(() => {
+                    timeout--;
+                    if (exports.isInitialized(options.package)) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                    if (timeout === 0) {
+                        reject(new Error("[Timeout] JS does not initialized in 2 seconds."));
+                    }
+                }, 10);
+            };
+            document.body.appendChild(scriptTag);
+        }
+    });
 };
-
-var nextTick = function(fn) { Promise.resolve().then(fn); };
-
-module.exports = {
-  isInitialized: isInitialized,
-  nextTick: nextTick,
-  loadJsPromise: loadJsPromise
+/**
+ * @hidden
+ */
+exports.nextTick = (fn) => {
+    Promise.resolve().then(fn);
 };
