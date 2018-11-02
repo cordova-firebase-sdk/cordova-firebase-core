7a11cd8fdcc6ee1261e0c70db6f9fad843bb6d6f
wf9a5m75 <wf9a5m75@gmail.com>
2018-11-01 18:27:15 -0700

    Import the class that defined in another plugin mandatory

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./src/www/App"));
+__export(require("./src/www/BaseArrayClass"));
+__export(require("./src/www/BaseClass"));
+__export(require("./src/www/common"));
+__export(require("./src/www/PluginBase"));
